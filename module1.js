8595b330a964fdbc3493a6767799e236638505e7
V3505 (Conference ID) <v3505@cam.ac.uk>
2015-08-14 11:05:37 +0100

    Photo Editor

--- module1.js
+++ module1.js
@@ -26,14 +26,143 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
+
+
 // function definitions here
+
+function makeMoreGreen(img, adjustment){
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i+=4){
+        data[i+1] = data[i+1] + adjustment;
+    }
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+
+function makeMoreBlue(img, adjustment){
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i+=4){
+        data[i+2] = data[i+2] + adjustment;
+    }
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+
+function makeMoreRed(img, adjustment){
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i+=4){
+        data[i+4] = data[i+4] + adjustment;
+    }
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+
+function brighten(img, adjustment){
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i+=4){
+        data[i] = data[i] + adjustment;
+        data[i+1] = data[i+1] + adjustment;
+        data[i+2] = data[i+2] + adjustment;
+    }
+
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+
+function makeInvert(img) {
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i+=4) {
+        data[i] = 255 - data[i];
+        data[i + 1] = 255-data[i + 1];
+        data[i + 2]= 255-data[i + 2];
+    }
+
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+
+function makeNoise(img, level){
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i=0; i < length; i+=4){
+        var adjustment = getRandomInt(-level, level);
+
+        data[i] = data[i] + adjustment;
+        data[i+1] = data[i+1] + adjustment;
+        data[i+2] = data[i+2] + adjustment;
+    }
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+function makeFunky(img) {
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length/2; i += 2) {
+        var temp = data[i];
+        data[i] = data[length - i];
+        data[length - i] = temp;
+    }
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
+function makeStripy (img){
+    var pixels = ImageUtils.getPixels(img);
+    var length = pixels.data.length;
+    var data = pixels.data;
+
+    for (var i = 0; i < length; i+=3){
+        data[i+1] = data[i+1] + 1000;
+    }
+    ImageUtils.putPixels(pixels, img.width, img.height);
+}
+
 
 
 $(document).ready(function() {
     var img = new Image();
     img.src = "img/cat.jpg";
+    //makeMoreBlue(img, 200);
+    //makeMoreGreen(img, 200);
+    //makeMoreRed(img, 200);
+    //brighten(img, 100);
+    //makeInvert(img);
+    //makeNoise(img, 50);
+    //makeFunky(img);
+    makeStripy(img);
+
+    /*var pixels = ImageUtils.getPixels(img);
+    var heightPixelsToHide = 100;
+    for(var i = 0; i < img.width * heightPixelsToHide * 4; i++) {
+        pixels.data[i] = 255;
+    }
 
 
-
+    console.log(pixels);
+    ImageUtils.putPixels(pixels, img.width, img.height);*/
 
 });
+
+
+
+
+
--- module3.js
+++ module3.js
@@ -87,6 +87,16 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function initializePixelGrid(height) {
+    var pixelGrid = [];
+    for(var y=0; y < height; y++) {
+        pixelGrid[y] = [];
+    }
+    return pixelGrid;
+}
+
+
+
 
 class RGBA {
     constructor(redValue, greenValue, blueValue, alphaValue) {
@@ -97,6 +107,61 @@ class RGBA {
     }
 }
 
+class ImageModel {
+    constructor(heightValue, widthValue, pixelGridValue){
+        this.height = heightValue;
+        this.width = widthValue;
+
+        if(pixelGridValue){
+            this.pixelGrid = pixelGridValue;
+        }
+        else{
+            this.pixelGrid = initializePixelGrid(heightValue);
+        }
+    }
+
+}
+
+function verticalMirror(imageModel) {
+    var mirrorImageModel = new ImageModel(imageModel.height, imageModel.width);
+
+    for (var y=0; y< imageModel.height; y++){
+        for (var x=0; x<imageModel.width/2; x++) {
+            var mirroredIndex = imageModel.width - 1 - x;
+            mirrorImageModel.pixelGrid[y][x] = imageModel.pixelGrid[y][mirroredIndex];
+            mirrorImageModel.pixelGrid[y][mirroredIndex] = imageModel.pixelGrid[y][x];
+        }
+    }
+    return mirrorImageModel;
+}
+
+
+function horizontalMirror(imageModel){
+    var mirrorImageModel = new ImageModel(imageModel.height, imageModel.width);
+
+    for (var y=0; y < imageModel.height/2; y++){
+        var mirroredIndex = imageModel.height - 1 - y;
+        for (var x=0; x<imageModel.width; x++) {
+            mirrorImageModel.pixelGrid[y][x] = imageModel.pixelGrid[mirroredIndex][x];
+            mirrorImageModel.pixelGrid[mirroredIndex][x] = imageModel.pixelGrid[y][x];
+        }
+    }
+    return mirrorImageModel;
+}
+
+function grayscale(imageModel) {
+    var grayscaleImageModel = new ImageModel(imageModel.height, imageModel.width);
+
+    for (var y = 0; y < imageModel.height; y++) {
+        for (var x=0; x < imageModel.width; x++) {
+            var rgba = imageModel.pixelGrid[y][x];
+            var average = (rgba.red + rgba.green + rgba.blue) / 3;
+            grayscaleImageModel.pixelGrid[y][x] = new RGBA(average, average, average,rgba.alpha);
+        }
+    }
+    return grayscaleImageModel;
+}
+
 
 // class definitions here
 
@@ -105,5 +170,14 @@ $(document).ready(function() {
     img.src = "img/cat.jpg";
 
 
+    var cat = ImageUtils.fromImgSrc("img/cat.jpg");
+    //ImageUtils.drawImageModel(cat);
+    //ImageUtils.drawImageModel(verticalMirror(cat));
+    //ImageUtils.drawImageModel(horizontalMirror(cat));
+    ImageUtils.drawImageModel(grayscale(verticalMirror(horizontalMirror(cat))));
+
+    /*var pixels = initializePixelGrid(10);
+    pixels[0][0] = new RGBA(255, 0, 0, 255);
+    var imageModel = new ImageModel(300, 400);*/
 
 });
